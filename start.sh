9d804da772e7dce06c6b9b9e0f9b95e726c69ea5
Garygunn94 <ggunn@tcd.ie>
2016-11-10 14:33:44 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 function int-ip { /sbin/ifconfig $1 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}'; }
-address=$(int-ip eth0)
+#address=$(int-ip eth0)
+address=$(localhost)
 port=${1-8080}
 
 ./Skeleton-Server $address $port
