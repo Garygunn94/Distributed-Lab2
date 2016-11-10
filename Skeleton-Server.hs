732853562354f0b77eefbd06448a63aa1955c756
Garygunn94 <ggunn@tcd.ie>
2016-11-10 14:20:59 +0000

    Made some edits to main

--- Skeleton-Server.hs
+++ Skeleton-Server.hs
@@ -9,11 +9,14 @@ import Control.Exception
 import Data.List.Split
 import Data.Word
 
+--Server data type allows me to pass address and port details easily
 data Server = Server { address :: String, port :: String }
 
+--Constructor
 newServer :: String -> String -> Server
 newServer addr portnum = Server { address = addr, port = portnum }
 
+--4 is easy for testing the pooling
 maxnumThreads = 4
 
 main:: IO ()
@@ -66,32 +69,39 @@ clientconnectHandler sock chan numThreads server = do
   count <- atomically $ readTVar numThreads
   putStrLn $ "numThreads = " ++ show count
 
-  --If there are still threads remaining...
+  --If there are still threads remaining create new thread and increment (exception if thread is lost -> decrement), else tell user capacity has been reached
   if (count < maxnumThreads) then do
     forkFinally (clientHandler handle chan server numThreads) (\_ -> atomically $ decrementTVar numThreads)
     atomically $ incrementTVar numThreads
     else do
-      hPutStrLn handle "Service reached maximum capacity, please try again later!"
+      hPutStrLn handle "Maximum number of threads in use. try again soon"
       hClose handle
 
   clientconnectHandler sock chan numThreads server
 
 clientHandler :: Handle -> Chan String -> Server -> TVar Int -> IO ()
 clientHandler handle chan server numThreads = do
+
+  --Recieve message from client
   line <- hGetLine handle
+  
+  --Seperate into words (seperated by whitespace)
   let cmd = words line
 
+  --Case switch HELO text and KILL_SERVICE are handled, everything else is ignored
   case (head cmd) of
     ("HELO") -> heloCommand handle chan server numThreads (unwords (tail cmd))
-    ("KILL_SERVICE") -> killCommand handle chan
-    _ -> do hPutStrLn handle ("Unknown Command - " ++ line)
+    ("KILL_SERVICE") -> killservice handle chan
+    _ -> do hPutStrLn handle ("Command not recognised - " ++ line)
   
   clientHandler handle chan server numThreads
 
+--Function called when HELO text command recieved  
 heloCommand :: Handle -> Chan String -> Server -> TVar Int -> String -> IO ()
 heloCommand handle chan server@Server{..} numThreads msg = do
-  writeChan chan "HELO command processed!"
+  writeChan chan "HELO command recieved"
 
+  --Sends the following to the client, as requested
   hPutStrLn handle $ "HELO " ++ msg ++ "\n\
                      \IP:" ++ address ++ "\n\
                      \Port:" ++ port ++ "\n\
@@ -99,13 +109,18 @@ heloCommand handle chan server@Server{..} numThreads msg = do
 
   hFlush handle
 
-killCommand :: Handle -> Chan String -> IO ()
-killCommand handle chan = do
-  hPutStrLn handle "Service is now terminating!"
+--Function called when KILL_SERVICE is recieved - Terminated the service
+killservice :: Handle -> Chan String -> IO ()
+killservice handle chan = do
+  --Let Clients know
+  hPutStrLn handle "Terminating the Service!"
+  --Send message up to mainHandler
   writeChan chan "KILL_SERVICE"
 
+--Increment Tvar stored in memory i.e. numThreads
 incrementTVar :: TVar Int -> STM ()
 incrementTVar tv = modifyTVar tv ((+) 1)
 
+--Decrement Tvar stored in memory i.e. numThreads
 decrementTVar :: TVar Int -> STM ()
 decrementTVar tv = modifyTVar tv (subtract 1)
